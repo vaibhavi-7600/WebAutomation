90d8084f1b15aaf4f0cdfcf5111ed0c3c81bcc94
vaibhaviGandhi <vaibhavi.gandhi@mintoak.com>
2023-05-01 15:07:40 +0530

    Added report

--- src/test/java/Base/TestBase.java
+++ src/test/java/Base/TestBase.java
@@ -1,15 +1,20 @@
 package Base;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 
 import java.util.concurrent.TimeUnit;
 
-public class TestBase {
 
+public class TestBase {
+    public ExtentReports report;
+    public ExtentTest extentTest;
     public WebDriver driver;
 
     @BeforeTest
@@ -31,5 +36,11 @@ public class TestBase {
 
         driver.close();
     }
+
+    @BeforeSuite
+    public void reportSetUp() {
+
+    }
+
 }
 
